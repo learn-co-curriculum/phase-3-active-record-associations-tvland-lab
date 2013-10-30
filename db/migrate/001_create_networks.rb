572037bd3dfe5cb5493df0221954fa1ada56dd43
Scott C. Reynolds <scott@scottcreynolds.com>
2013-10-30 13:09:05 -0400

    add more instructions to migrations

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -3,7 +3,6 @@
 #by convention, the class name should match the part of the
 #file name after the number, so in this case:
 #001_create_networks.rb becomes class CreateNetworks
-
 class CreateNetworks < ActiveRecord::Migration
   #define a change method in which to do the migration
   def change
--- db/migrate/002_create_shows.rb
+++ db/migrate/002_create_shows.rb
@@ -1,3 +1,6 @@
+# DO NOT EDIT!
+# If you need to add things to shows, you need to create a new migration file
+# and additively change the schema. Always add new migrations to change the db.
 class CreateShows < ActiveRecord::Migration
   def change
     create_table :shows do |t|
