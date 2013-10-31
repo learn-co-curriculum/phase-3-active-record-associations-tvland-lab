68f63cdd533bfd4e659269c1e3493a98381fe4f6
Scott C. Reynolds <scott@scottcreynolds.com>
2013-10-31 10:53:22 -0400

    fixing load paths

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,7 @@ require 'active_record'
 
 Bundler.require
 
-require_relative '../app/models/network.rb'
-require_relative '../app/models/show.rb'
-require_relative '../app/models/character.rb'
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
