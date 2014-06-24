cd3a2261f42e83f4136650ad33327ada32b37d95
ahimmelstoss <ahimmelstoss@gmail.com>
2014-06-24 09:51:31 -0400

    solutions/migrations for all tests; rspec 3 syntax changes on tests

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,14 +1,13 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-  has_many :shows, through: :characters
 
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
 
   def list_roles
-    characters.collect do |character|
+    self.characters.collect do |character| 
       "#{character.name} - #{character.show.name}"
-    end
+    end.join("\n") 
   end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,4 @@
 class Show < ActiveRecord::Base
   has_many :characters
-  has_many   :actors, through: :characters
   belongs_to :network
 end
