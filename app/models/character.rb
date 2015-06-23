9e01871c1293f6c637bf111fa4c49f177e2a3646
deniznida <denizunat@hotmail.com>
2015-06-23 13:28:58 -0400

    modify solution

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,10 +2,6 @@ class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
 
-  def show_name
-    show ? show.name : "Unknown"
-  end
-
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
