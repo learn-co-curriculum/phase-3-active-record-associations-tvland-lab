4ea762372840f553627cf9a8218af8899f46b438
gabe <gj@users.noreply.github.com>
2016-07-24 13:11:56 -0400

    Mullaney --> Mulaney

--- app/models/network.rb
+++ app/models/network.rb
@@ -2,6 +2,6 @@ class Network < ActiveRecord::Base
   has_many :shows
 
   def sorry
-    "We're sorry about passing on John Mullaney's pilot"
+    "We're sorry about passing on John Mulaney's pilot"
   end
 end
