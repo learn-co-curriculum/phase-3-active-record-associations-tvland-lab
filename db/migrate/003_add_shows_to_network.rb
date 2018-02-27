81d66ca219f318316b4b5299bc44df9ba4fdafcf
curiositypaths <curiositypaths@users.noreply.github.com>
2018-02-27 12:07:08 -0500

    Add AR version declaration

--- db/migrate/003_add_shows_to_network.rb
+++ db/migrate/003_add_shows_to_network.rb
@@ -1,4 +1,4 @@
-class AddShowsToNetwork < ActiveRecord::Migration
+class AddShowsToNetwork < ActiveRecord::Migration[5.1]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
