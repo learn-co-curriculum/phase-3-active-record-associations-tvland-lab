b04e0f8c1b5010cdc7c84d1560e830c523cb1ef9
curiositypaths <curiositypaths@users.noreply.github.com>
2018-02-27 12:07:08 -0500

    Add AR version declaration

--- db/migrate/006_add_actor_id_to_characters.rb
+++ db/migrate/006_add_actor_id_to_characters.rb
@@ -1,4 +1,4 @@
-class AddActorIdToCharacters < ActiveRecord::Migration
+class AddActorIdToCharacters < ActiveRecord::Migration[5.1]
   def change
     add_column :characters, :actor_id, :integer
   end
