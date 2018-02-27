f8cae829a055407f304d1fba961645e0e307ec90
curiositypaths <curiositypaths@users.noreply.github.com>
2018-02-27 12:07:08 -0500

    Add AR version declaration

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -3,7 +3,7 @@
 #by convention, the class name should match the part of the
 #file name after the number, so in this case:
 #001_create_networks.rb becomes class CreateNetworks
-class CreateNetworks < ActiveRecord::Migration
+class CreateNetworks < ActiveRecord::Migration[5.1]
   #define a change method in which to do the migration
   def change
     create_table :networks do |t| #we get a block variable here for the table
--- db/migrate/002_create_shows.rb
+++ db/migrate/002_create_shows.rb
@@ -1,7 +1,7 @@
 # DO NOT EDIT!
 # If you need to add things to shows, you need to create a new migration file
 # and additively change the schema. Always add new migrations to change the db.
-class CreateShows < ActiveRecord::Migration
+class CreateShows < ActiveRecord::Migration[5.1]
   def change
     create_table :shows do |t|
       #primary key of :id is created for us!
