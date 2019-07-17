84ecff873ed6396d105d7d03756d3d1d206a0530
maxwellbenton <maxwellbenton@gmail.com>
2019-07-17 13:19:50 -0400

    updates actors_list solution

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,9 @@
 class Show < ActiveRecord::Base
   has_many :characters
-  has_many   :actors, through: :characters
   belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.actors.map{|actor| actor.full_name}
+  end
 end
