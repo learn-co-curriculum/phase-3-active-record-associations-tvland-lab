611013c29d064927b0f288682b13c3aef0e96c96
Rob Cobb <rwcobbjr@gmail.com>
2019-11-26 14:48:23 -0500

    normalize db setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,8 @@
 require "bundler/setup"
-
 require "sinatra/activerecord"
-
 Bundler.require
+require_all 'app/models'
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+ENV['SINATRA_ENV'] ||= 'development'
 
-connection_details = YAML::load(File.open('config/database.yml'))
-ActiveRecord::Base.establish_connection(connection_details)
+ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
