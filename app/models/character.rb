0f94b9cf0bdc8f04e7a8e6545de1952b46e888aa
Daryl Rogony <darylrogony@gmail.com>
2022-06-19 22:48:37 +0300

    commit tree 7536479ff65f72fc8d1fce4ef889cc00966717d7

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :characters do |t|
+      t.string :name 
+      t.integer :actor_id
+      t.integer :show_id
+    end
   end
 end
