627fcacd0370aa9714bc7513baea14d3518df48b
Shree Pandey <sridhar.pandey1@gmail.com>
2022-10-18 03:45:46 -0500

    commit tree 5441197a59bb3ada7e4297b0327898d7175df26c

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,12 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    
+      t.timestamps
+    end
+    
   end
 end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,13 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+    
+      t.timestamps
+    end
+    
   end
 end
