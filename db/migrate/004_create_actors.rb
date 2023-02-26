10be52cf1d9670f347c9a789bd6cdf51d76ca8d7
justinekipkoech <justinekipkoech98@gmail.com>
2023-02-26 22:32:38 +0300

    commit tree c137ae577d445f64b265ab45d57fd456973b8809

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,9 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,6 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    add_column :characters, :catchphrase, :string
   end
 end
