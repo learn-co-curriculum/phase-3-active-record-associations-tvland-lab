695fc89947c5448965ec322327f21e4f3bc04260
Meshack Korir <meshackkorir100@gmail.com>
2023-06-10 11:24:27 +0300

    coded actor

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    characters.map{ |character| "{character.name} - #{character.show.name}"}
+  end  
 end
